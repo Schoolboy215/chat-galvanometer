8e3d2f684523e03df812484b61b7652698692880
James McKay <james.mckay215@yahoo.com>
2026-04-19 09:49:28 -0700

    Add crash log

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,36 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System.IO;
 using System.Windows;
+using System.Windows.Threading;
 using Application = System.Windows.Application;
 
 namespace ChatGalvanometer;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        DispatcherUnhandledException += (s, args) =>
+        {
+            WriteCrashLog(args.Exception);
+            args.Handled = true;
+            Shutdown(1);
+        };
+
+        AppDomain.CurrentDomain.UnhandledException += (s, args) =>
+            WriteCrashLog((Exception)args.ExceptionObject);
+
+        base.OnStartup(e);
+    }
+
+    private static void WriteCrashLog(Exception ex)
+    {
+        try
+        {
+            var dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ChatGalvanometer");
+            Directory.CreateDirectory(dir);
+            File.WriteAllText(Path.Combine(dir, "crash.log"), $"{DateTime.Now}\n{ex}");
+        }
+        catch { }
+    }
 }
 
